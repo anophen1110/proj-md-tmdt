4ca24677b8fdcf0640fd064cbfb63ce77cac6cdc
Manh Dung <125356880+MD2dm@users.noreply.github.com>
2023-06-29 23:09:44 +0700

    update

--- src/views/404/index.tsx
+++ src/views/404/index.tsx
@@ -1,13 +1,22 @@
+import ButtonAll from '@components/button-account';
 import NavBar from '@layoutStore/nav';
 import TitleHead from '@layoutStore/title-head';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Page404 = () => {
     return (
         <>
             <TitleHead></TitleHead>
             <NavBar></NavBar>
-            this is 404
+            <div className='centerAll flex-col gap-y-5 mt-[150px]'>
+                <div className='text-[110px] fontAcount'>404 Not Found</div>
+                <div className=''>Your visited page not found. You may go home page.</div>
+                <NavLink to="/" className='mt-20'>
+                    <ButtonAll textColor='text-white' nameButton='Back to home page' background='bg-[#DB4444]'></ButtonAll>
+                </NavLink>
+            </div>
+
         </>
     );
 };
